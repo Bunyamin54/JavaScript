d23992f97419282c251af4f9eaa72230f9b0ec8c
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-02 21:29:15 +0200

    src script

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+confirm("emin misiniz");
+// alert kodu uygulamayi bloklar.
+
+console.warn("Bu bir uyaridir.");
+
+karar = confirm("Emin msiniz E/H");
+console.log(karar);
+
+console.warn("Bu bir uyarıdir");
+
+console.error("Eyvah hata olustu..");
