5b6e613382ad87ad9fdb2e14b5154bff0b89ce01
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-02 22:09:18 +0200

    undefined

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,30 @@
-confirm("emin misiniz");
-// alert kodu uygulamayi bloklar.
+// confirm("emin misiniz");
+// // alert kodu uygulamayi bloklar.
 
-console.warn("Bu bir uyaridir.");
+// console.warn("Bu bir uyaridir.");
 
-karar = confirm("Emin msiniz E/H");
-console.log(karar);
+// karar = confirm("Emin msiniz E/H");
+// console.log(karar);
 
-console.warn("Bu bir uyarıdir");
+// console.warn("Bu bir uyarıdir");
 
-console.error("Eyvah hata olustu..");
+// console.error("Eyvah hata olustu..");
+
+//? ====================== Const ==============
+
+console.log("APP JS RUNNING");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+//! caught SyntaxError: Missing initializer in const declaration (at app.js:27:7)
+// const deneme; //? const ile tanimlanan degiskene baslangic degeri verilmelidir.
+// console.log(deneme)
+
+//! Uncaught TypeError: Assignment to constant variable.
+const number1 = 5;
+number1 = 7; //? const ile tanimlanan degiskene sonradan atama yapilamaz
