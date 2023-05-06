09bbf5661439c08b32d011548b53acf3fb362b90
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-06 10:31:39 +0200

    aritmetik

--- js1-intro/js02-operator/operators.js
+++ js1-intro/js02-operator/operators.js
@@ -1,1 +1,22 @@
 console.log("Js dosyası bağlandı.");
+
+var deneme = 5;
+
+varDeneme = 6;
+
+let myVar = 5;
+
+const myVar1 = 6;
+
+const num1 = 10;
+const num2 = 69;
+
+const toplam = num1 + num2;
+
+console.log(toplam);
+
+const msg1 = "Hello";
+const msg2 = "World";
+
+const mesaj = msg1 + msg2;
+console.log(mesaj);
