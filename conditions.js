5ab4fac5968c0f4ff5dc88e9b7298b7f3965d269
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-08 20:35:38 +0200

    multi condition

--- conditions.js
+++ conditions.js
@@ -15,3 +15,20 @@ if (calismaSuresi >= 10) {
   console.log("Uzgunuz maasiniza zam yapamiyoruz.", maas);
 }
 console.log("Gule gule");
+
+//? cok kosullu ornek
+
+const grade = prompt("Please enter your grade:");
+let result = ""
+
+if (grade < 40) {
+  result = "FF";
+} else if (grade <= 50) {
+  result = "DD";
+} else if (grade <= 65) {
+  result = "CC";
+} else if =(grade <=89) {
+    result ="BB"
+} else {
+    result = "AA"
+}
