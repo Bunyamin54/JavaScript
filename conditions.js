e24d11495c1285ce15e7ecbedb076cca2fd69413
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-08 20:07:25 +0200

    link

--- conditions.js
+++ conditions.js
@@ -3,3 +3,13 @@
 // * ===============================================
 
 console.log("****** CONDITIONS *******");
+
+const calismaSuresi = prompt("Calisma suresi");
+let maas = prompt("Maasinizi giriniz");
+
+if (calismaSuresi >= 10) {
+  maas = maas * 1.1;
+  console.log("Zamli maasiniz", maas);
+} else {
+  console.log("Uzgunuz maasiniza zam yapamiyoruz.");
+}
