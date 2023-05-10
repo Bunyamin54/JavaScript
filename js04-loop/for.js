f2f7c875ca9aed89c2098222cf9174cfb676680b
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-10 21:26:10 +0200

    kommentarer

--- js04-loop/for.js
+++ js04-loop/for.js
@@ -18,3 +18,21 @@ for (let i = 1; i <= 100; i++) {
 //? cont kullanmak forun icinde bizim faydamiza olur.
 
 //!Math.random rastegele sayi atar, Math.round ile sayiyi yuvarlariz.
+//? while da once kosul / sonra dongu ici / sonra artis ++
+
+//! rakamli adetli durumlarda for sytaxdi best praktis
+//? karmasik kosullarin iterasyonu icin while veya do while dongusu best practis sytax.
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+//? ODEV1: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
+
+//* NOT:
+//* 1 ve kendisinden başka böleni olmayan sayılar ASAL sayılardır.
+//* Eğer girilen sayı, herhangi bir sayıya tam bölünüyorsa diğer sayıları kontrol etmeye gerek yoktur. ASAL DEĞİL diyebiliriz.
+//* break deyimi ile dongu bir koşul gerçekleştiğinde kırılabilir.
