6ce9a5a760668cad5f7670ac87fdf75efd4737db
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-10 19:54:16 +0200

    break og continue

--- js04-loop/for.js
+++ js04-loop/for.js
@@ -1,1 +1,11 @@
-https://uiano.zoom.us/j/63316963224
+// * =======================================================
+// *             FOR LOOP
+// * =======================================================
+
+console.log(" **** FOR ****");
+
+for (let i = 1; i <= 100; i++) {
+  console.log("Cohort 14");
+}
+// break - donguyu kirar
+// continue - donguyu teget gecer.
