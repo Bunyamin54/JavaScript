96be6b1524347fce3f580d4c1d5f5ffbc8e51e23
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-13 10:51:31 +0200

    functions

--- js05-functions1/declaration.js
+++ js05-functions1/declaration.js
@@ -3,3 +3,13 @@
 // ? ======================================
 
 console.log("** FUNC DECLARATION **");
+
+//!Declaration  (Fonksiyonun tanimlanmasi.)
+
+function yazdirMerhaba() {
+  //   alert("Merhaba FS14");
+  console.log("Merhaba FS14");
+}
+yazdirMerhaba(); //!invoke call
+yazdirMerhaba(); //!invoke call
+yazdirMerhaba(); //!invoke call
