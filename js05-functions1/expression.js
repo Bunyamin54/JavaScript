8259f24d3b18637da75ac325abcd6a71f22e295f
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-13 13:00:20 +0200

    arrow yontemi

--- js05-functions1/expression.js
+++ js05-functions1/expression.js
@@ -18,3 +18,13 @@ let oddEven = function (number) {
 
 console.log(oddEven(24));
 console.log(oddEven(23));
+
+//* ARROW yontemi
+
+const kareA = (x) => x * x;
+
+kareA(4);
+
+//* x - prametre => arrow  - x*x govdesi  // Arrow funksjionlarin otomatik return ozelligi var
+
+//*Arrow funksjunlar tek satirlik islevler icin daha elverisli.
