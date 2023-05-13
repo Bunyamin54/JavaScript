60f259568862f3290266183cd00533ad9c139926
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-13 12:52:34 +0200

    odd even

--- js05-functions1/expression.js
+++ js05-functions1/expression.js
@@ -3,9 +3,18 @@
 // ? ======================================
 
 console.log("** EXPRESSION**");
+
 //? Deklarasyon  once tanimlama sonra yazdirma faydalidir.
+
 const yazdir = function (message) {
   console.log(message);
 };
 
 yazdir("Hallo FS14");
+
+let oddEven = function (number) {
+  return number % 2 === 0 ? "EVEN" : "ODD"; //* onemli bir kod
+};
+
+console.log(oddEven(24));
+console.log(oddEven(23));
