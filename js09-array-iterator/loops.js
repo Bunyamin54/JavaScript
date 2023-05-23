3cd1f594ee07fdb4642c1680b4342ff6d33a2371
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-23 11:50:39 +0200

    math round

--- js09-array-iterator/loops.js
+++ js09-array-iterator/loops.js
@@ -6,8 +6,16 @@ console.log("**** LOOPS IN ARRAYS ***");
 
 //?-------------- ÖRNEK -------------------
 //? grades'in ortalamasini hesaplayiniz.
-// const grades = [55, 77, 23, 89, 100, 44, 33, 45]
+const grades = [55, 77, 23, 89, 100, 44, 33];
 
+let sum = 0;
+for (let i = 0; i < grades.length; i++) {
+  sum += grades[i];
+}
+
+console.log("AVG", Math.round(sum / grades.length));
+
+//! Math. round  sayiyi yuvarlar.
 // let sum = 0
 // for (let i = 0; i < grades.length; i++) {
 //   sum += grades[i]
@@ -16,19 +24,19 @@ console.log("**** LOOPS IN ARRAYS ***");
 
 // console.log("AVG:", (sum / grades.length).toFixed(2))
 
-//?-------------- ÖRNEK -------------------
-//? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayri diziye kaydediniz.
-const grades = [55, 77, 23, 89, 100, 44, 33, 45];
-const lessThan50 = [];
-const equalOrBiggerThan50 = [];
+// //?-------------- ÖRNEK -------------------
+// //? grades dizisindeki notların 50'den kucuk olanları ve buyuk olanları 2 ayri diziye kaydediniz.
+// const grades = [55, 77, 23, 89, 100, 44, 33, 45];
+// const lessThan50 = [];
+// const equalOrBiggerThan50 = [];
 
-for (let j = 0; j < grades.length; j++) {
-  if (grades[j] < 50) {
-    lessThan50.push(grades[j]);
-  } else {
-    equalOrBiggerThan50.push(grades[j]);
-  }
-}
-console.log(lessThan50);
-console.log(equalOrBiggerThan50);
-console.log(grades);
+// for (let j = 0; j < grades.length; j++) {
+//   if (grades[j] < 50) {
+//     lessThan50.push(grades[j]);
+//   } else {
+//     equalOrBiggerThan50.push(grades[j]);
+//   }
+// }
+// console.log(lessThan50);
+// console.log(equalOrBiggerThan50);
+// console.log(grades);
