bab7eebb1e2d1c07a2cf26ceaebede37758a0b5d
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-23 11:59:30 +0200

    lessThan - equalOrBigger

--- js09-array-iterator/loops.js
+++ js09-array-iterator/loops.js
@@ -8,15 +8,29 @@ console.log("**** LOOPS IN ARRAYS ***");
 //? grades'in ortalamasini hesaplayiniz.
 const grades = [55, 77, 23, 89, 100, 44, 33];
 
-let sum = 0;
-for (let i = 0; i < grades.length; i++) {
-  sum += grades[i];
-}
+// let sum = 0;
+// for (let i = 0; i < grades.length; i++) {
+//   sum += grades[i];
+// }
 
-console.log("AVG", (sum / grades.length).toFixed(2));
+// console.log("AVG", (sum / grades.length).toFixed(2));
+
 //? toFixed 2 son iki degeri alir
-
 //! Math. round  sayiyi yuvarlar.
+
+const lessThan50 = [];
+const equalOrBiggerThan50 = [];
+
+for (let j = 0; j < grades.length; j++) {
+  if (grades[j] < 50) {
+    lessThan50.push(grades[j]);
+  } else {
+    equalOrBiggerThan50.push(grades[j]);
+  }
+}
+console.log(lessThan50);
+console.log(equalOrBiggerThan50);
+console.log(grades);
 // let sum = 0
 // for (let i = 0; i < grades.length; i++) {
 //   sum += grades[i]
