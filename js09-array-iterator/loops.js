5eaa9d5a4d64dd9c779d70f79e22288dc54b67a9
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-23 12:09:43 +0200

    for in loop

--- js09-array-iterator/loops.js
+++ js09-array-iterator/loops.js
@@ -6,7 +6,7 @@ console.log("**** LOOPS IN ARRAYS ***");
 
 //?-------------- ÖRNEK -------------------
 //? grades'in ortalamasini hesaplayiniz.
-const grades = [55, 77, 23, 89, 100, 44, 33];
+// const grades = [55, 77, 23, 89, 100, 44, 33];
 
 // let sum = 0;
 // for (let i = 0; i < grades.length; i++) {
@@ -28,14 +28,31 @@ const grades = [55, 77, 23, 89, 100, 44, 33];
 //     equalOrBiggerThan50.push(grades[j]);
 //   }
 // }
+// console.log(lessThan50);
+// console.log(equalOrBiggerThan50);
+// console.log(grades);
 //! lessThan methodu kucukse rakamdan
 //* equalOrBiggerThan methodu esit veya buyukse
 //*ekleme methodu push ile yapilir
 
 //!
-// console.log(lessThan50);
-// console.log(equalOrBiggerThan50);
-// console.log(grades);
+
+//? for in dongusu
+
+const grades = [55, 77, 23, 89, 100, 44, 33];
+const lessThan50 = [];
+const equalOrBiggerThan50 = [];
+
+for (let i in grades) {
+  grades[i] < 50
+    ? lessThan50.push(grades[i])
+    : equalOrBiggerThan50.push(grades[i]);
+}
+
+console.log(lessThan50);
+console.log(equalOrBiggerThan50);
+console.log(grades);
+
 // let sum = 0
 // for (let i = 0; i < grades.length; i++) {
 //   sum += grades[i]
