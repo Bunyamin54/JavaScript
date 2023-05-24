da5967515fd5b0405258600274d1964670199769
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 20:24:59 +0200

    objects key value

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -9,4 +9,14 @@
 //? Object'lerde Key-value (property-value) yapisi kullanilir.
 //? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
 
+//*  key - value var
 console.log("******* Objects *********");
+
+//* . notasyonu  car.brand = "togg"
+//* clg(car.model)
+const car = {
+  brand: "mercedes",
+  model: "2020",
+  engine: 2000,
+  speed: 5,
+};
