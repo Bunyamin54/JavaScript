ccd7241e29649193b73836378bcc7fc8dfea84c3
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 20:38:30 +0200

    objects

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -14,9 +14,26 @@ console.log("******* Objects *********");
 
 //* . notasyonu  car.brand = "togg"
 //* clg(car.model)
+// const car = {
+//   brand: "mercedes",
+//   model: "2020",
+//   engine: 2000,
+//   speed: 5,
+// };
+// console.log(car.model);
+
+//! object literea, en cok tercih edilen yontem
 const car = {
-  brand: "mercedes",
-  model: "2020",
+  brand: "Mercedes",
+  model: 2020,
   engine: 2000,
   speed: 5,
+  color: ["pink", "black", "blue", "green"],
 };
+
+console.log(car);
+console.log(car.engine);
+console.log(car.color[0]);
+
+console.log(car.color);
+car.color.forEach((c) => console.log(c));
