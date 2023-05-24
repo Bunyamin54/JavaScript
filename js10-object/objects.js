6b85d322d8ded8a6c48bf5a9df9a0f54e92c0d2c
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 21:50:51 +0200

    arrow object

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -86,3 +86,73 @@ const person1 = new Personel("123345456", "Ahmet Can", 33, 30000);
 const person2 = new Personel("124859045", "Hasan Kelek", 35, 10000);
 
 console.log(person1);
+
+//* ---------------------------------------------------------
+//* 2- Object() class'ından new Operatörü ile
+//* ---------------------------------------------------------
+
+const myCar = new Object();
+myCar.brand = "Mercedes";
+myCar.model = 2022;
+myCar.speed = 7;
+
+console.log(myCar.speed);
+console.log(myCar["model"]);
+
+console.log(myCar);
+
+//* ---------------------------------------------------------
+//* 3- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+//* ---------------------------------------------------------
+
+function Personel(id, name, age, salary) {
+  this.id = id;
+  this.name = name;
+  this.age = age;
+  this.salary = salary;
+  console.log(this);
+}
+
+const person1 = new Personel("1234567890", "Ahmet Can", 33, 30000);
+const person2 = new Personel("123456745454", "Mehmet Veli", 23, 10000);
+
+console.log(person1);
+
+// //? ES6
+// class Personel{
+//     constructor()
+// }
+
+//* ======================================================
+//*              Object Metotlari
+//* ======================================================
+
+const personel = {
+  name: "Ahmet",
+  surname: "Canan",
+  dob: 1990,
+  salary: 30000,
+  drivingLicense: true,
+  calculateAge: function () {
+    console.log(this); //? personel objesini refere eder.
+    return new Date().getFullYear() - this.dob;
+  },
+  summary: () => {
+    console.log(this); //? window objesini refere eder.
+    return `${this.name} is ${this.calculateAge()} years old`;
+  },
+};
+
+//! NOT: arrow fonksiyonlari ozellikle callback fonksiyonu olarak
+//! kullanilmak ve bu fonksiyonlarda this keyword kullanim
+//! gereksinimini kaldirmak icin gelistirilmistir.
+//! Lexical context'e sahiptirler.Dolayisiyla, bir obje fonksiyonu
+//! olarak kullanilirsa, this kelimesi global scope'u (window nesnesini)
+//! gösterir. Bunu engellemek için object fonksiyonlarini tanimlarken
+//! diger (func. expression veya declaration) yontemlerini kullanabilir.
+
+console.log(personel.name + " " + personel.surname);
+
+console.log("AGE:", personel.calculateAge());
+
+console.log(personel.summary());
