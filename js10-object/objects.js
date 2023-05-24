1b462a7e151b1449550bf3a89ba70132e1227601
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 21:26:18 +0200

    object construction

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -71,3 +71,18 @@ console.log(myCar.speed);
 console.log(myCar.brand);
 
 console.log(myCar);
+//* ---------------------------------------------------------
+//* 3- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+//* ---------------------------------------------------------
+//! bir kere yaz new operatoru ile seri uretim icin bu yapi object constructuru yapsisi deniliyor.
+function Personel(id, name, age, salary) {
+  this.id = id;
+  this.name = name;
+  this.age = age;
+  this.salary = salary;
+}
+
+const person1 = new Personel("123345456", "Ahmet Can", 33, 30000);
+const person2 = new Personel("124859045", "Hasan Kelek", 35, 10000);
+
+console.log(person1);
