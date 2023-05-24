6cc411ca7848d85277fa160434d4661cebdda8fc
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 22:16:17 +0200

    objects

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -25,34 +25,34 @@ console.log("******* Objects *********");
 //! object literea, en cok tercih edilen yontem
 // * reed method
 
-const car = {
-  brand: "Mercedes",
-  model: 2020,
-  engine: 2000,
-  speed: 5,
-  color: ["pink", "black", "blue", "green"],
-};
+// const car = {
+//   brand: "Mercedes",
+//   model: 2020,
+//   engine: 2000,
+//   speed: 5,
+//   color: ["pink", "black", "blue", "green"],
+// };
 
-console.log(car);
-console.log(car.engine);
-console.log(car.color[0]);
+// console.log(car);
+// console.log(car.engine);
+// console.log(car.color[0]);
 
-console.log(car.color);
-car.color.forEach((c) => console.log(c));
-//! write method
+// console.log(car.color);
+// car.color.forEach((c) => console.log(c));
+// //! write method
 // //? objenin degerini degistirme (write)
 
-car.speed = 7;
-car.color = "Morcivert";
-car.class = "E";
+// car.speed = 7;
+// car.color = "Morcivert";
+// car.class = "E";
 
-console.log(car);
+// console.log(car);
 
-// * Square Bracket yontemi
+// // * Square Bracket yontemi
 
-console.log("ENGINE", car["engine"]);
+// console.log("ENGINE", car["engine"]);
 
-console.log("ENGINE:", car["engine"]); //? objeAdi["keyAdi"]
+// console.log("ENGINE:", car["engine"]); //? objeAdi["keyAdi"]
 
 // const myKey = prompt(
 //   "Araba ile ilgili Neyi ogrenmek istersiniz: speed, color,engine ..."
@@ -62,61 +62,61 @@ console.log("ENGINE:", car["engine"]); //? objeAdi["keyAdi"]
 
 //* object (classindan new operaotoru ile)
 
-const myCar = new Object();
-myCar.brand = "BMW";
-myCar.model = 2023;
-myCar.speed = 7;
+// const myCar = new Object();
+// myCar.brand = "BMW";
+// myCar.model = 2023;
+// myCar.speed = 7;
 
-console.log(myCar.speed);
-console.log(myCar.brand);
+// console.log(myCar.speed);
+// console.log(myCar.brand);
 
-console.log(myCar);
+// console.log(myCar);
 //* ---------------------------------------------------------
 //* 3- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
 //* ---------------------------------------------------------
-//! bir kere yaz new operatoru ile seri uretim icin bu yapi object constructuru yapsisi deniliyor.
-function Personel(id, name, age, salary) {
-  this.id = id;
-  this.name = name;
-  this.age = age;
-  this.salary = salary;
-}
+// //! bir kere yaz new operatoru ile seri uretim icin bu yapi object constructuru yapsisi deniliyor.
+// function Personel(id, name, age, salary) {
+//   this.id = id;
+//   this.name = name;
+//   this.age = age;
+//   this.salary = salary;
+// }
 
-const person1 = new Personel("123345456", "Ahmet Can", 33, 30000);
-const person2 = new Personel("124859045", "Hasan Kelek", 35, 10000);
+// const person1 = new Personel("123345456", "Ahmet Can", 33, 30000);
+// const person2 = new Personel("124859045", "Hasan Kelek", 35, 10000);
 
-console.log(person1);
+// console.log(person1);
 
 //* ---------------------------------------------------------
 //* 2- Object() class'ından new Operatörü ile
 //* ---------------------------------------------------------
 
-const myCar = new Object();
-myCar.brand = "Mercedes";
-myCar.model = 2022;
-myCar.speed = 7;
+// const myCar = new Object();
+// myCar.brand = "Mercedes";
+// myCar.model = 2022;
+// myCar.speed = 7;
 
-console.log(myCar.speed);
-console.log(myCar["model"]);
+// console.log(myCar.speed);
+// console.log(myCar["model"]);
 
-console.log(myCar);
+// console.log(myCar);
 
 //* ---------------------------------------------------------
 //* 3- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
 //* ---------------------------------------------------------
 
-function Personel(id, name, age, salary) {
-  this.id = id;
-  this.name = name;
-  this.age = age;
-  this.salary = salary;
-  console.log(this);
-}
+// function Personel(id, name, age, salary) {
+//   this.id = id;
+//   this.name = name;
+//   this.age = age;
+//   this.salary = salary;
+//   console.log(this);
+// }
 
-const person1 = new Personel("1234567890", "Ahmet Can", 33, 30000);
-const person2 = new Personel("123456745454", "Mehmet Veli", 23, 10000);
+// const person1 = new Personel("1234567890", "Ahmet Can", 33, 30000);
+// const person2 = new Personel("123456745454", "Mehmet Veli", 23, 10000);
 
-console.log(person1);
+// console.log(person1);
 
 // //? ES6
 // class Personel{
@@ -127,21 +127,21 @@ console.log(person1);
 //*              Object Metotlari
 //* ======================================================
 
-const personel = {
-  name: "Ahmet",
-  surname: "Canan",
-  dob: 1990,
-  salary: 30000,
-  drivingLicense: true,
-  calculateAge: function () {
-    console.log(this); //? personel objesini refere eder.
-    return new Date().getFullYear() - this.dob;
-  },
-  summary: () => {
-    console.log(this); //? window objesini refere eder.
-    return `${this.name} is ${this.calculateAge()} years old`;
-  },
-};
+// const personel = {
+//   name: "Ahmet",
+//   surname: "Canan",
+//   dob: 1990,
+//   salary: 30000,
+//   drivingLicense: true,
+//   calculateAge: function () {
+//     console.log(this); //? personel objesini refere eder.
+//     return new Date().getFullYear() - this.dob;
+//   },
+//   summary: () => {
+//     console.log(this); //? window objesini refere eder.
+//     return `${this.name} is ${this.calculateAge()} years old`;
+//   },
+// };
 
 //! NOT: arrow fonksiyonlari ozellikle callback fonksiyonu olarak
 //! kullanilmak ve bu fonksiyonlarda this keyword kullanim
@@ -151,8 +151,24 @@ const personel = {
 //! gösterir. Bunu engellemek için object fonksiyonlarini tanimlarken
 //! diger (func. expression veya declaration) yontemlerini kullanabilir.
 
-console.log(personel.name + " " + personel.surname);
+// console.log(personel.name + " " + personel.surname);
 
-console.log("AGE:", personel.calculateAge());
+// console.log("AGE:", personel.calculateAge());
 
-console.log(personel.summary());
+// console.log(personel.summary());
+
+// * JSON  => Javascript Object Notation
+// * obje ve dizi formatidir. bir dizi icersindeki obje yapilarina Json denir
+// *[ {}, {}, {}], ornek   {
+// *    name:
+//  *   surname:
+//   *  age:
+//* }
+
+const team = {
+  name: "Ahmet",
+  surname: "can",
+  job: "developer",
+};
+
+console.log(team);
