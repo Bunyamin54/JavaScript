d3a93142a2acbb3818c5e228b64fab9dd8b31ccc
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-24 22:18:05 +0200

    objects

--- js10-object/objects.js
+++ js10-object/objects.js
@@ -164,11 +164,51 @@ console.log("******* Objects *********");
 //  *   surname:
 //   *  age:
 //* }
+/********************************************************
+//* JSON => Javascript Object Notation
+//********************************************************
 
-const team = {
-  name: "Ahmet",
-  surname: "can",
-  job: "developer",
-};
+const team = [
+  { name: "Ahmet", surname: "Can", job: "Developer", age: 30 },
+  { name: "Mary", surname: "Bary", job: "tester", age: 22 },
+  { name: "Hazel", surname: "Nut", job: "developer", age: 20 },
+]
 
-console.log(team);
+console.log(team)
+console.log(team[1])
+console.log(team[1].name) //? Mary
+console.log(team[2].age) //? 20
+
+console.log("************")
+
+//* Ornek1: team dizisindeki job'lari tek tek yazdiriniz.
+team.forEach((p) => console.log(p.job))
+
+//* Ornek2: age'leri bir artirarak yeni bir diziye saklayiniz.
+const personelAges = team.map((person) => person.age + 1)
+console.log(personelAges)
+
+//* Ornek3: name ve surname'leri birlestirip buyuk harfe ceviren ve
+//* bunu fullName key'i olarak saklayan, ayni zamanda age degerlerini 5
+//* arttirarak age key'ine saklayan ve olusan diziyi donduren kodu yazınız.
+
+const teamFullName = team.map((p) => {
+  return {
+    fullName: p.name.toUpperCase() + " " + p.surname.toUpperCase(),
+    age: p.age + 5,
+  }
+})
+
+//? Alternative way
+const teamFullName1 = team.map((p) => ({
+  fullName: p.name.toUpperCase() + " " + p.surname.toUpperCase(),
+  age: p.age + 5,
+}))
+
+console.log(teamFullName1)
+
+//* Ornek4: Yasi(age) 22 'den kucuk esit olan kisilerin adlarini (name) listeyiniz.
+
+//* Ornek5: 22 yasindan kucuk ve esit olanlarin isimlerini diziye saklayiniz.
+
+//* Ornek6: ortalama yasi hesaplayiniz.
