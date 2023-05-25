404578ab3d5a7be3c77d0fa0f7a0556c402108c3
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-25 19:23:01 +0200

    New Generation Operators

---
+++ js11_new-generation operator/newops.js
@@ -0,0 +1,13 @@
+//* ======================================================
+//*  DESTRUCTURING (OBJECT)
+//* ======================================================
+console.log("****** NEW OPERATORS *******");
+
+const car = {
+  brand: "BMW",
+  model: 2020,
+  engine: 2000,
+  colors: ["pink", "blue"],
+};
+
+console.log(car.brand);
