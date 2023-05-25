3ac13a3afcbe1a0c26268d330170c807cf1f6628
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-25 19:39:42 +0200

    destructure model

--- js11_new-generation operator/newops.js
+++ js11_new-generation operator/newops.js
@@ -9,14 +9,46 @@ const car = {
   engine: 2000,
   colors: ["pink", "blue"],
 };
-//? 1. yontem (. notasyonu)
 
-const brand = car.brand;
-console.log(brand);
+// //? 1.Yontem (. notasyon)
+// const brand = car.brand
+// console.log(brand)
 
-//? 2. yontem (square bracket)
-console.log(car["model"]);
+// //? 2.Yontem (square bracket)
+// console.log(car["model"])
 
-//? 3. Destructure  // sart degiskenlerin ismi object destructure
-const { brands, engine, colors } = car;
-console.log(engine, colors, brands);
+// //? 3. Destructure
+// const { brand: name, engine, colors } = car //? object destr.
+// console.log(engine, colors, name)
+
+// const { model } = car
+// console.log(model)
+
+//* EXAMPLE: NESTED
+const cars = {
+  car1: {
+    name: "BMW",
+    model: 1990,
+    engine: 1.6,
+  },
+  car2: {
+    name: "Mercedes",
+    model: 2022,
+    engine: 2.0,
+  },
+  car3: ["anadol", "togg", "devrim"],
+  car4: "kağnı",
+};
+
+let { car1, car2, car4, car3 } = cars; //? 1 seviye destr.
+console.log(car2, car4);
+
+const { name: c1Name, model: c1Model } = car1; //? 2 seviye dest.
+const { name: c2Name, model: c2Model } = car2;
+console.log(c2Name, c2Model);
+
+//? Alternativly
+const {
+  car1: { nameCar1, modelCar1, engineCar1 },
+  car2: { nameCar2, modelCar2, engineCar2 },
+} = cars;
