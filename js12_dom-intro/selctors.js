a4b00c103acb084c6cd27e7a455df3739c21092f
Bunyamin54 <https://github.com/Bunyamin54>
2023-05-30 20:30:00 +0200

    add.buton

--- js12_dom-intro/selctors.js
+++ js12_dom-intro/selctors.js
@@ -8,6 +8,30 @@ console.log(myHeader);
 
 myHeader.style.backgroundColor = "red";
 myHeader.style.color = "white";
+//* ADD Buton
+
+const addButon = document.getElementById("btn");
+console.log(addButon);
+
+addButon.style.backgroundColor = " #bebe";
+addButon.style.border = "none";
+addButon.style.borderRadius = "1opx";
+addButon.style.padding = "0.6rem";
+
+// ? deger atama
+addButon.value = "SAVE";
+
+//? h1
+const h1 = document.getElementById("heading1");
+console.log(h1);
+
+//? Deger atama
+
+h1.textContent = "DOM Introductions";
+
+//? deger okuma
+console.log(h1.textContent);
+
 //*===========================================
 //*          GETELEMENTSBYTAGNAME()
 //*===========================================
