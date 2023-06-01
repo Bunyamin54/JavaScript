13dbc074cae00a391d71ef22ec16c7cfab841f54
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-01 19:30:36 +0200

    document created

--- js13_createElement/crateNode.js
+++ js13_createElement/crateNode.js
@@ -1,1 +1,18 @@
 console.log("**** CREATE NODE ****")
+
+//?1 olustur
+const langInput = document.querySelector("#input")
+//! olustur creat et 
+const newP = document.createElement("p")
+
+//? 2 eger text tabanli bir element ise text dugumunu olustur
+const text = document.createTextNode("langInput.value")
+
+//? 3- olusturulan text dugumunu yeni elemente bagla
+newP.appendChild(text)
+
+
+console.log(newP);
+
+//! 2 bagla ata (append)
+
