edd34a5beb51b79ea1054e9e3d20099055e9e730
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-10 16:04:36 +0200

    else if

--- Projects/project_check-out/app.js
+++ Projects/project_check-out/app.js
@@ -1,1 +1,20 @@
-console.log("hello");
+
+// ******selectors
+// const minus = document.querySelectorAll(".fa-minus")
+// const plus = document.querySelectorAll(".fa-plus")
+// console.log(minus);
+const main = document.querySelector(".main__product")
+
+main.addEventListener("click", (e) => {
+    console.log(e.target.classList);
+
+    if(e.target.classList.contains("fa-minus")) {
+        if(e.target.nextElementSibling.innerText > 1) {
+            e.target.nextElementSibling.innerText --
+        }
+    } else if (e.target.classList.contains("fa-plus")) {
+
+       e.target.previousElementSibling.innerText ++;
+
+    }
+})
