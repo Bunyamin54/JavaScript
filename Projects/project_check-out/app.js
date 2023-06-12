d41b6d67a3b85f2557e989090ca9d6bbfea79f54
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-12 14:35:41 +0200

    check out

--- Projects/project_check-out/app.js
+++ Projects/project_check-out/app.js
@@ -1,6 +1,8 @@
 // ******selectors
 const main = document.getElementById("products-preview")
-document.getElementsByClassName
+const amountOfProducts = document.querySelector(".amount-of-product")
+const selectedProducts = document.querySelector(".main__sum-price")
+// let totalAmount = 0
 // ******** events
 main.addEventListener("click", (e) => {
     if(e.target.classList.contains("fa-minus")) {
@@ -13,7 +15,22 @@ main.addEventListener("click", (e) => {
         e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
     } else if (e.target.classList.contains("fa-trash-can")){
         if (confirm("are you sure???") == true){
-       e.target.closest(".main__product").remove()
+       e.target.closest(".main__product").remove();
+       amountOfProducts.innerText --
     }else {}
     }
+    selectedProducts.innerHTML = updateAmount()
 })
+// function subTotal(position) {
+//     e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.{position}.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
+// }
+function updateAmount () {
+    let totalAmount = document.getElementsByClassName("main__product-line-price");
+    // selectedProducts.innerText = totalAmount
+    let sum = 0;
+    for(let i of totalAmount){
+         sum += +(i.innerText); //sum = sum + Number(i.innerText)
+        // console.log(i.innerText);
+    }
+    return sum;
+}
