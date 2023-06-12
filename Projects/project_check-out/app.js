488545f4c413b8fe0ce408721ed2b3ca7374e3bc
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-12 13:00:16 +0200

    check out project

--- Projects/project_check-out/app.js
+++ Projects/project_check-out/app.js
@@ -1,18 +1,19 @@
-// const main = document.querySelector(".main__product")
+// ******selectors
 const main = document.getElementById("products-preview")
 document.getElementsByClassName
+// ******** events
 main.addEventListener("click", (e) => {
-    // console.log(e.target.classList);
     if(e.target.classList.contains("fa-minus")) {
         if(e.target.nextElementSibling.innerText > 1) {
             e.target.nextElementSibling.innerText --
+            e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.nextElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
         }
     } else if (e.target.classList.contains("fa-plus")) {
         e.target.previousElementSibling.innerText ++;
-        e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".main__product-line-price").innerText = e.target.previousElementSibling.innerText * e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".dollar").innerText
-        // e.target.previousElementSibling.innerText
-        // e.target.parentElement.parentElement.parentElement.parentElement.querySelector(".dollar").innerText
+        e.target.closest(".main__product").querySelector(".main__product-line-price").innerText = (e.target.previousElementSibling.innerText * e.target.closest(".main__product-info").querySelector(".dollar").innerText).toFixed(2)
     } else if (e.target.classList.contains("fa-trash-can")){
-       e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+        if (confirm("are you sure???") == true){
+       e.target.closest(".main__product").remove()
+    }else {}
     }
 })
