85efe09d98317fd2edb776767712bd53bc66cb7b
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 01:09:37 +0200

    counter

--- Kodluyoruz.org/Java_script/js_04-number-veri-tur/number.js
+++ Kodluyoruz.org/Java_script/js_04-number-veri-tur/number.js
@@ -3,3 +3,21 @@ let tax = 0.18
 let priceTax = price* tax
 let total = price + priceTax
 console.log("fiyat",price, "kdv orani" ,tax, "kdv tutari", priceTax , "fiyat", total);
+
+
+let counter = 320
+
+counter = counter +1
+console.log(counter);
+
+counter += 1
+console.log(counter);
+
+counter ++;
+console.log(counter);
+
+counter --;
+console.log(counter);
+
+counter *=10
+console.log(counter);
