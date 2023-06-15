027c7567932eb6c5deda26cd2ce7982af100ded1
Bunyamin Genc <bunyaming@uia.no>
2023-06-15 11:41:55 +0200

    alert

---
+++ Kodluyoruz.org/Java_script/js_01-intro/index.js
@@ -0,0 +1,12 @@
+// **Live reload enabled.
+// console.log(Merhaba Dunya)
+// VM175:1 Uncaught SyntaxError: missing ) after argument list
+// console.log("Merhaba dunya")
+// VM200:1 Merhaba dunya
+// undefined
+// alert("Merhaba Dunya")
+// undefined
+// document.write("Merhaba Dunya")
+// undefined 
+console.log("Hello World");
+
