e73324578793196a2cfbaf27777badc6cc7b4eab
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 01:35:07 +0200

    boolean

---
+++ Kodluyoruz.org/Java_script/js_05-boolean/boolaen.js
@@ -0,0 +1,23 @@
+//* o ve 1 anlamak
+
+let isActive = false //* 0
+isActive = true // * 1
+console.log(isActive);
+
+
+let userName;
+let isUserName= Boolean(userName)
+console.log(isUserName);
+
+Boolean ("11") // true
+Boolean ("0") // true
+Boolean ("") // false
+
+userName = "user"
+console.log("User name var mi :", Boolean(userName));
+
+Boolean (0) //false
+Boolean (-0) // false
+Boolean (-0.1) // true  // deger oldugu icin
+Boolean (0 === 0)// true 
+
