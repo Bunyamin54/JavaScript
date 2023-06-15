f0282242df239a20e75db8e340e7cf7b11d80859
Bunyamin Genc <bunyaming@uia.no>
2023-06-15 11:57:00 +0200

    first declarations

--- Kodluyoruz.org/Java_script/js_02-degisken_tanimlama/index.js
+++ Kodluyoruz.org/Java_script/js_02-degisken_tanimlama/index.js
@@ -1,2 +1,18 @@
-var serverName = "kodluyoruz.org "
+// var serverName = "kodluyoruz.org "
+// console.log(serverName);
+
+let serverName ;
+console.log(serverName); //* undefined
+
+serverName = "https://kodluyoruz.org"
 console.log(serverName);
+
+let password = 1234
+console.log(password);
+
+// console.log(fullname); 
+ // index.js:13 Uncaught ReferenceError: Cannot access 'fullname' before initialization
+// at index.js:13:13
+// (anonymous) @ index.js:13
+let fullname = "Bunyamin Genc"
+console.log(fullname); // Bunyamin Genc  //* tanimlama oncesinde yapilmali
