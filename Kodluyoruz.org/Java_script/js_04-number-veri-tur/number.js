e1e23b8e7ff19fbe360fa74f75d6c3d25d0e0bc8
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 01:14:11 +0200

    % mod

--- Kodluyoruz.org/Java_script/js_04-number-veri-tur/number.js
+++ Kodluyoruz.org/Java_script/js_04-number-veri-tur/number.js
@@ -25,7 +25,18 @@ console.log(counter);
 counter /= 2
 console.log(counter);
 
+console.log(2+3*10);  //* 32
+console.log((2+3)*10); //* 50  islem onceligi parantez
 
-console.log(2+3*10);
+//* mod kalan alma %
+
+//* sayi tek mi cift mi 
+
+console.log(14 % 2); //* 1 kalani verir 2 ye bolup  sifir ise cift 1 ise tek
+
+console.log("Koli kalan urun ornegi:",18 % 8 );
+
+
+
 
 
