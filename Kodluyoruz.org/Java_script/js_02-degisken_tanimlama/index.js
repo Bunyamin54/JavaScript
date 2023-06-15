57720def65ca9e3aa1702c36304472e69d17b518
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 00:22:06 +0200

    const

--- Kodluyoruz.org/Java_script/js_02-degisken_tanimlama/index.js
+++ Kodluyoruz.org/Java_script/js_02-degisken_tanimlama/index.js
@@ -1,22 +1,41 @@
 // var serverName = "kodluyoruz.org "
 // console.log(serverName);
 
-let serverName ;
-console.log(serverName); //* undefined
+// let serverName ;
+// console.log(serverName); //* undefined
 
-serverName = "https://kodluyoruz.org"
-console.log(serverName);
+// serverName = "https://kodluyoruz.org"
+// console.log(serverName);
 
-let password = 1234
-console.log(password);
+// let password = 1234
+// console.log(password);
 
 // console.log(fullname); 
  // index.js:13 Uncaught ReferenceError: Cannot access 'fullname' before initialization
 // at index.js:13:13
 // (anonymous) @ index.js:13
-let fullName = "Bunyamin Genc"
-fullName= "Lorem ipsum dollor"  //* let i birden fazla tanimlayabiliriz.
-console.log(fullName); // Bunyamin Genc  //* tanimlama oncesinde yapilmali
+// let fullName = "Bunyamin Genc"
 
-fullName  + "Yeni eklenen bilgi"
-console.log(fullName + "Test Bilgisi");
+// fullName= "Lorem ipsum dollor" 
+//  //* let i birden fazla tanimlayabiliriz.
+// console.log(fullName); // Bunyamin Genc  //* tanimlama oncesinde yapilmali
+// ** birlestirme veya ekleme
+
+// fullName  + "Yeni eklenen bilgi"
+// console.log(fullName + " Test Bilgisi");
+
+// fullName = fullName + " Yeni Bilgi"
+
+// console.log(fullName);
+
+
+// fullName = "Sifirladik"
+
+// fullName += " ve yeni bilgi eklendi"
+// console.log(fullName);
+
+const SERVER_PASSWORD = "selam"
+
+console.log(SERVER_PASSWORD);
+
+// * const bir kere tanimlanir ve degismez. 
