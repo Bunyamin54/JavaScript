ee19f5ef3f968b1911b9a08333c4ce37eede7fcd
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 00:32:23 +0200

    turkce

--- Kodluyoruz.org/js_03-degiskenler/degiskenler.js
+++ Kodluyoruz.org/js_03-degiskenler/degiskenler.js
@@ -2,3 +2,11 @@
 
 let fullName = ""
 let kodluyoruzServerInfo = ""
+
+// ** UPPER_CASE  kullanimi  buyuk harflerde alt cizgi ile birlestiriyoruz.
+
+const PASSWORD = "1234"
+const SERVER_PASSWORD = 12345
+
+//* Degiskenler turkce karakterler icermemeli
+
