d955d6dbb084243f7f86bfe7bf0d920c29f12d9c
Bunyamin Genc <bunyaming@uia.no>
2023-06-15 20:51:55 +0200

    const

--- js18_asymc2/async-await.js
+++ js18_asymc2/async-await.js
@@ -18,3 +18,28 @@
 //* satirdaki kodun durdurulmasini saglar.
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
+const getNews = async () => {
+    const API_KEY = "1a1a999e0d7240a6bd2dead87bcca78e"
+  
+    const URL = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`
+  
+    try {
+      const res = await fetch(URL)
+      if (!res.ok) {
+        throw new Error("News can not be fetched")
+      }
+      const data = await res.json()
+      renderNews(data.articles)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  const renderNews = (news) => {
+    console.log(news)
+  }
+  
+  window.addEventListener("load", () => {
+    getNews()
+  })
+  
