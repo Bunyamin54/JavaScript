10b60bbf4a5cb177cf7cba06566034d455d6e1b6
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 10:41:01 +0200

    template

--- Kodluyoruz.org/Java_script/js_07-template-literates/index.js
+++ Kodluyoruz.org/Java_script/js_07-template-literates/index.js
@@ -5,10 +5,12 @@ let email = userName + " @ " + DOMAIN
 console.log("Merhaba", userName, "sitemize hosgeldin", "mail adresin", email);
 
 // * template literates yontemi   ${} ile daha fazla bilgi tutabiliriz sistemimizde
-
+// * yeni yazim tarzi 
 let info = `Merhaba ${userName} sitemize hosgeldin.
- mail adresin ${ email},mail adresinin uzunlugi${email.length}
- borcunuz ${ (2 + 3) * 5} tl
- gunun saat bilgisi ${new Date().getHours()}
+ 
+Kisa isminiz: ${userName[0]}.
+Mail adresin ${ email},mail adresinin uzunlugi${email.length}
+Borcunuz ${ (2 + 3) * 5} tl
+Gunun saat bilgisi ${new Date().getHours()}
 `
 console.log(info);
