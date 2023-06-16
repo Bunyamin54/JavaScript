7a44224e63c38e8f4237159a078cfe9a7a317729
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 10:06:44 +0200

    typeof

---
+++ Kodluyoruz.org/js_06-veri-turler/index.js
@@ -0,0 +1,20 @@
+// * veri turunu ogrenmek icin typeof
+
+let price = 111
+let strinPrice ="111"
+let hasPassword = true
+
+console.log(
+" price :",
+typeof(price));
+
+
+console.log(
+    " stringPrice:",
+    typeof(strinPrice));
+
+    
+console.log(
+    " hasPassword :",
+    typeof(hasPassword));
+    
