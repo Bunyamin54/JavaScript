ed0b25555896eef394a6cfbb974a45feab4f0771
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 11:22:30 +0200

    pathName HostName

--- Kodluyoruz.org/Java_script/js_09-dom/index.js
+++ Kodluyoruz.org/Java_script/js_09-dom/index.js
@@ -2,3 +2,5 @@ console.log(document.location);
 console.log(document.body);
 console.log(document.head);
 console.log(document.URL);
+console.log(document.location.hostname);
+console.log(document.location.pathname);
