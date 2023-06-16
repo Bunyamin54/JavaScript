f42d4532e6cf9161fd141f8744dd268f88f97827
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 10:38:59 +0200

    template $

--- Kodluyoruz.org/Java_script/js_07-template-literates/index.js
+++ Kodluyoruz.org/Java_script/js_07-template-literates/index.js
@@ -6,7 +6,9 @@ console.log("Merhaba", userName, "sitemize hosgeldin", "mail adresin", email);
 
 // * template literates yontemi   ${} ile daha fazla bilgi tutabiliriz sistemimizde
 
-let info = `Merhaba ${userName} sitemize hosgeldin . mail adresin ${ email},mail adresinin uzunlugi${email.length}
+let info = `Merhaba ${userName} sitemize hosgeldin.
+ mail adresin ${ email},mail adresinin uzunlugi${email.length}
+ borcunuz ${ (2 + 3) * 5} tl
+ gunun saat bilgisi ${new Date().getHours()}
 `
-
 console.log(info);
