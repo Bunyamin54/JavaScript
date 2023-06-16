774f76899aa15a3cae81bc3257b12356231c5832
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 11:46:53 +0200

    innerHTML

--- Kodluyoruz.org/Java_script/js_09-dom/index.js
+++ Kodluyoruz.org/Java_script/js_09-dom/index.js
@@ -1,11 +1,27 @@
-console.log(document.location);
-console.log(document.body);
-console.log(document.head);
-console.log(document.URL);
-console.log(document.location.hostname);
-console.log(document.location.pathname);
+// console.log(document.location);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.URL);
+// console.log(document.location.hostname);
+// console.log(document.location.pathname);
 
 // ** Document Object Model
 
 document.body.style.backgroundColor = "greenyellow"
 
+// let title = document.getElementById("title")
+
+// console.log(title);
+
+
+let title =document.getElementById("title")
+title.innerHTML = "Degisen bilgi"
+console.log(title.innerHTML) // * degisen bilgi 
+
+
+let link= document.querySelector("a")
+// link.innerHTML = "Link bilgisi degisti"
+link.innerHTML += " degisti"
+// console.log(link.innerHTML);
+link.style.color ="red"
+link.classList.add("btn")
