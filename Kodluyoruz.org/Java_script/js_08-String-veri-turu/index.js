04b0748e020d262ca7fee70fa6eed86ee5429ca9
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 10:49:44 +0200

    search

---
+++ Kodluyoruz.org/Java_script/js_08-String-veri-turu/index.js
@@ -0,0 +1,19 @@
+let email= "hakanyalcinkaya@gmail.com"
+
+let firstName = "Hakan"
+let lastName = "Yalcinkaya"
+// *  onemli bir yapi tipi length.
+console.log(email.length);
+console.log(firstName[0]);
+console.log(firstName.charAt(1)); //* ilk bilgisine ulasmak icin ve sifrdan baslar
+
+// * buyuk kucuk harf
+firstName = firstName.toUpperCase()
+console.log(firstName);
+
+firstName= firstName.toLowerCase()
+console.log(firstName);
+
+// * string icerisindeki bilgi aratmak icin search 
+
+
