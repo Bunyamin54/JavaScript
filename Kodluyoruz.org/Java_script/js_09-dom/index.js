2e2e347855e5c3fabf1b4bb0a473a0ecf647a3c6
Bunyamin Genc <bunyaming@uia.no>
2023-06-16 11:48:27 +0200

    innerHtml

--- Kodluyoruz.org/Java_script/js_09-dom/index.js
+++ Kodluyoruz.org/Java_script/js_09-dom/index.js
@@ -7,21 +7,21 @@
 
 // ** Document Object Model
 
-document.body.style.backgroundColor = "greenyellow"
+//* document.body.style.backgroundColor = "greenyellow"
 
-// let title = document.getElementById("title")
+//* let title = document.getElementById("title")
 
-// console.log(title);
-
-
-let title =document.getElementById("title")
-title.innerHTML = "Degisen bilgi"
-console.log(title.innerHTML) // * degisen bilgi 
+// *console.log(title);
 
 
-let link= document.querySelector("a")
-// link.innerHTML = "Link bilgisi degisti"
-link.innerHTML += " degisti"
-// console.log(link.innerHTML);
-link.style.color ="red"
-link.classList.add("btn")
+// *let title =document.getElementById("title")
+// *title.innerHTML = "Degisen bilgi"
+// *console.log(title.innerHTML) // * degisen bilgi 
+
+
+//* let link= document.querySelector("a")
+// *link.innerHTML = "Link bilgisi degisti"
+// *link.innerHTML += " degisti"
+// *console.log(link.innerHTML);
+// *link.style.color ="red"
+// *link.classList.add("btn")
