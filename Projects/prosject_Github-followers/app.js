96ea86e909d0c0b7c38b8489a9484f337ec2b3b9
Bunyamin Genc <bunyaming@uia.no>
2023-06-17 11:07:09 +0200

    serach button

---
+++ Projects/prosject_Github-followers/app.js
@@ -0,0 +1,19 @@
+const searchInput = document.getElementById("searchText")
+
+const searchBtn = document.querySelector("#button")
+
+const cardsDiv = document.querySelector(".cards")
+
+
+console.log(searchInput);
+console.log(searchBtn);
+console.log(cardsDiv);
+
+
+searchBtn.addEventListener("click",() => {
+
+
+console.log(searchInput.value);
+
+
+})
