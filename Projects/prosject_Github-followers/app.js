c651bea7f9d82665b4c44c859ce972d6816dfeb2
Bunyamin Genc <bunyaming@uia.no>
2023-06-17 11:33:42 +0200

    fetch

--- Projects/prosject_Github-followers/app.js
+++ Projects/prosject_Github-followers/app.js
@@ -1,25 +1,37 @@
-const searchInput = document.getElementById("searchText")
-
-const searchBtn = document.querySelector("#button")
-
-const cardsDiv = document.querySelector(".cards")
-
+//javascript
+const searchInput = document.getElementById("searchText");
+const searchBtn = document.querySelector("#button");
+const cardsDiv = document.querySelector(".cards");
 
 console.log(searchInput);
 console.log(searchBtn);
 console.log(cardsDiv);
 
+searchBtn.addEventListener("click", () => {
+  if (searchInput.value) {
+    console.log(searchInput.value);
+    getData(searchInput.value);
+  }else{
+    alert("Lütfen bir kullanıcı adı giriniz")
+  }
+});
 
-searchBtn.addEventListener("click",() => {
-
-    if (searchInput.value) {
-        console.log(searchInput.value);
-    }else {
-        alert("Lutfen bir kullanici adi giriniz!")
+async function getData(username){
+    console.log(username);
+    const url = `https://api.github.com/users/${username}/followers?per_page=100`;
+    try {
+        let response = await fetch(url);
+        if(response.ok){
+            const data = await response.json()
+            console.log(data);
+            
+        }
+        
+        
+    } catch (error) {
+        console.log(error);
+        
     }
-
-
-
-
-
-})
+      
+    
+}
