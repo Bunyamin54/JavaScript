1e6e85bb24f1d54e696cf1afbbd9db698aa7b8bc
Bunyamin Genc <bunyaming@uia.no>
2023-06-19 12:25:50 +0200

    img

--- 30 days 30 project/day1/weather.js
+++ 30 days 30 project/day1/weather.js
@@ -16,8 +16,6 @@ e.currentTarget.reset()
     // console.log(e.target)
 })
 
-
-
 const getData = async () => {
 const API_KEY = "929ef530f519c46e95ec303b405eb183"
 const cityName = input.value 
@@ -26,6 +24,42 @@ const units = "metric"
 
 const URL = `https://api.openweathermap.org/data/2.5/weather?q
 =${cityName}Oslo&appid=${API_KEY}929ef530f519c46e95ec303b405eb183&units
-=${units}metric&lang=no `
+=${units}&lang=no `
+
+
+try {
+
+    const res = await fetch(URL)
+
+
+ if (!res.ok){
+throw new Error(res.status)
+
+ }
+const data =await res.json()
+console.log(data)
+} catch (error) {
+    
+}
+
+
 
 }
+
+
+//* try catch  yakaliyor-- calismaya devam eder. hatayi atliyor sonraki kodu yazdiyor
+// try {
+//     let array
+
+// array.forEach(element => {
+//     console.log("first")
+// });
+
+
+    
+// } catch (error) {
+    
+// }
+
+
+// console.log("sonraki kod")
