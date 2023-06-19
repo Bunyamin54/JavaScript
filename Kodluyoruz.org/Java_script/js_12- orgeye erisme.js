10add147817937f8e2a2d3e0f7e9ef9818bb0603
Bunyamin Genc <bunyaming@uia.no>
2023-06-19 10:37:20 +0200

    first child

---
+++ Kodluyoruz.org/Java_script/js_12- orgeye erisme.js
@@ -0,0 +1,21 @@
+//*  liste icersindeki son ve ilk elemanlara ulasmak
+// let item = document.querySelector("ul#list>li:last-child").innerHTML  //* Laborum
+
+// console.log(item)
+
+// let item = document.querySelector("ul#list>li:last-child")
+
+// item.innerHTML = "Son oge degisti"
+
+
+
+
+//let item1 = document.querySelector("ul#list>li:first-child").innerHTML  //* lorem
+
+//console.log(item1)
+
+
+// let firstChild = document.querySelector("ul#list>li:first-child")
+// firstChild.innerHtml = "ilk oge degisti"
+
+
