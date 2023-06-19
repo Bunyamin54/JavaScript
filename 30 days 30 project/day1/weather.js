d639e09e10200cac7d6e1638e3fd0668d0db7d18
Bunyamin Genc <bunyaming@uia.no>
2023-06-19 11:41:36 +0200

    api

--- 30 days 30 project/day1/weather.js
+++ 30 days 30 project/day1/weather.js
@@ -9,7 +9,23 @@ form.addEventListener("submit", (e) => {
 // * form.reset()
 // * e.target.reset()
 
+
+getData()
 e.currentTarget.reset()
 
-    console.log(e.target)
+    // console.log(e.target)
 })
+
+
+
+const getData = async () => {
+const API_KEY = "929ef530f519c46e95ec303b405eb183"
+const cityName = input.value 
+const units = "metric"
+
+
+const URL = `https://api.openweathermap.org/data/2.5/weather?q
+=${cityName}Oslo&appid=${API_KEY}929ef530f519c46e95ec303b405eb183&units
+=${units}metric&lang=no `
+
+}
