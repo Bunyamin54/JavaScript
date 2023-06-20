1411012137d80311677a15d9f252ecdf3c9bd6f7
Bunyamin Genc <bunyaming@uia.no>
2023-06-20 22:08:20 +0200

    export import

--- js20_oop/js20_modules/index.js
+++ js20_oop/js20_modules/index.js
@@ -1,7 +1,18 @@
 console.log("JS Modules is running")
 //? modul kullanimnda modul objesi this nesnesini gostermez.
-
+//? named export (import) deniyor bu sisteme
 console.log(this) // undefined
 
 console.log(window)
 
+import {CALISMA_SAATI, maas} from "./mymodule.js"
+import {arttir, azalt} from "./mymodule.js"
+ 
+//? export default  suslu kullanilmaz suslu parentezsiz
+import selamVer from "./mymodule.js"
+
+console.log(CALISMA_SAATI)
+console.log(maas)
+console.log(arttir(5))
+console.log(azalt(5))
+console.log(selamVer())
--- js20_oop/js20_modules/mymodule.js
+++ js20_oop/js20_modules/mymodule.js
@@ -1,1 +1,23 @@
 console.log("My Module Loaded")
+
+export const  CALISMA_SAATI = 40
+
+export let maas = 20000
+ 
+
+ let sayac = 0
+ export const arttir =(miktar) => {
+sayac += miktar
+return sayac
+
+ }
+
+ export const azalt =(miktar) => {
+    sayac -= miktar
+    return sayac
+     }
+//? herbir modul icin sadece bir tane default eksport olabilir.
+     export default function selamVer() {
+
+        alert("MerhabaFS-14")
+     }
