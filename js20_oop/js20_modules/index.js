5af1483a3e91494feacd539c67e294eb52fbd29c
Bunyamin Genc <bunyaming@uia.no>
2023-06-20 22:11:35 +0200

    export default

--- js20_oop/js20_modules/index.js
+++ js20_oop/js20_modules/index.js
@@ -6,13 +6,20 @@ console.log(this) // undefined
 console.log(window)
 
 import {CALISMA_SAATI, maas} from "./mymodule.js"
-import {arttir, azalt} from "./mymodule.js"
+import {arttir as inc, azalt as dec} from "./mymodule.js"
  
 //? export default  suslu kullanilmaz suslu parentezsiz
-import selamVer from "./mymodule.js"
+// import selamVer from "./mymodule.js"
+import mesajVer from "./mymodule.js"
 
 console.log(CALISMA_SAATI)
 console.log(maas)
-console.log(arttir(5))
-console.log(azalt(5))
+// console.log(arttir(5))
+// console.log(azalt(5))
+//! export default 
+console.log(inc(5))
+console.log(dec(5))
+
 console.log(selamVer())
+
+mesajVer()
