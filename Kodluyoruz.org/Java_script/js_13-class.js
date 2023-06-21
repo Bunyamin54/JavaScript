0227da586acb03d4b0a8ef84f12c21612c358c5d
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 12:18:27 +0200

    remove classList

--- Kodluyoruz.org/Java_script/js_13-class.js
+++ Kodluyoruz.org/Java_script/js_13-class.js
@@ -7,4 +7,9 @@ greeting.classList.add("text-primary")
 greeting.classList.add("title")
 greeting.classList.add("new-info", "second-class", "third-clas")
 
+
+//? silme 
+greeting.classList.remove( "second-class", "third-clas")
+
+
 console.log(greeting.classList)
