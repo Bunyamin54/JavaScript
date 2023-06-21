2c6eaa27c5d9514ee026a8f976d6f36c7bb33e02
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 14:57:16 +0200

    ternary

---
+++ Kodluyoruz.org/Java_script/js_15-ternary.js
@@ -0,0 +1,7 @@
+let userName = prompt("Kullanici bilginiz yaziniz")
+let info = document.querySelector("#info")
+
+//* ternary kullanimi  
+//* kosul ? doruysa : yanlissa
+
+info.innerHTML = ` ${userName.length > 0 ? userName : "Kullanici bilginiz bulunamadi :("} `
