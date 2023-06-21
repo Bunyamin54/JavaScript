f558ec7784b9b2bf9410fe6fea7d90b6074f330c
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 14:19:12 +0200

    promt

---
+++ Kodluyoruz.org/Java_script/if.js
@@ -0,0 +1,13 @@
+//*  if conditions
+
+//* eger kullanici bilgisi varsa ekrana ismini yazdir.
+
+
+//* eger (username.length >0) {console.log (username)}
+
+let userName=prompt("Kullanici adinizi giriniz")
+
+if (userName.length > 0) {
+
+    console.log(`Kullanici bilginiz ${userName}`)
+}
