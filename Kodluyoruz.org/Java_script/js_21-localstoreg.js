41d69ff245da88eebada10bcfdade2cba3e0184b
Bunyamin Genc <bunyaming@uia.no>
2023-06-22 00:27:58 +0200

    get set item localStorage

---
+++ Kodluyoruz.org/Java_script/js_21-localstoreg.js
@@ -0,0 +1,19 @@
+localStorage.setItem('myCat', 'Kod Kedisi')
+
+localStorage.getItem('myCat')
+
+// * getItem getirir setItem atar - ve key value ilsikisi vardir.
+
+localStorage.removeItem('myCat') //* remove ile de sileriz
+
+let user= {userName :" Hakanyalcinkaya, isActive : true"}
+
+localStorage.setItem('userInfo', JSON.stringify(user))
+
+let userInfo= localStorage.getItem('userInfo')
+console.log(userInfo)
+
+JSON.parse(userInfo)
+
+user.isActive
+userInfo.isActive
