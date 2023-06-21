674a342eb71b962c654a4fede59aef848cf1d890
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 14:41:57 +0200

    if else

--- Kodluyoruz.org/Java_script/if.js
+++ Kodluyoruz.org/Java_script/if.js
@@ -5,11 +5,37 @@
 
 //* eger (username.length >0) {console.log (username)}
 
-let userName=prompt("Kullanici adinizi giriniz")
+// let userName=prompt("Kullanici adinizi giriniz")
 
-if (userName.length) {
+// if (userName.length) {
 
-    console.log(`Kullanici bilginiz ${userName}`)
-}else {
-    console.log("bilgiyok")
-}
+//     console.log(`Kullanici bilginiz ${userName}`)
+// }else {
+//     console.log("bilgiyok")
+// }
+
+let userName= prompt("Kullanici Adiniz")
+let age = prompt("Yasiniz")
+let info = document.querySelector("#info")
+
+// if (userName && age >= 18) {
+
+//     console.log("Ehliyet alabilirsiniz")
+// }else if (!userName){
+//     console.log("Kullanici adiniz yok")
+// }else if ( !(age >= 18) ) {
+
+
+//     console.log("Yas bilginiz yok veya 18 yasindan kucuksunuz")
+// }
+
+if (userName && age >= 18) {
+
+    info.innerHTML = "Ehliyet alabilirsiniz" 
+}else if (!userName){
+    info.innerHTML = "Kullanici adiniz yok"
+}else if ( !(age >= 18) ) {
+
+
+    info.innerHTML = "Yas bilginiz yok veya 18 yasindan kucuksunuz"
+} 
