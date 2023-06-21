070e6f77b3c46d0e876265ff90ffbe3a81ea312b
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 14:11:50 +0200

    let && ||

---
+++ Kodluyoruz.org/Java_script/js_14-operators.js
@@ -0,0 +1,20 @@
+console.log("operatorler")
+let price = "100"
+let user= "hakan"
+
+console.log(price == 1)
+console.log(price === 100)
+
+console.log(price != 1)  //* esit degilse
+
+
+//* != esit degilse
+console.log(user != "quest")
+
+//* && ve
+
+price = 0
+
+console.log(price && user !="quest")
+
+console.log(price > 0 || user !="quest")
