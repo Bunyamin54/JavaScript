9eaecba91de9d722aadd6932db6f12ccb54f5a5d
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 21:20:37 +0200

    functions

---
+++ Kodluyoruz.org/Java_script/js_17-fonction.js
@@ -0,0 +1,19 @@
+//* ilk fonksiyon
+
+function hello() {
+
+    console.log("merhaba")
+    helloWorld()
+}
+
+
+//! ic ice fonksiyon yapisi olusturabiliriz. 
+//! foknsiyon istedigimiz yerde cagirabiliyotz 
+
+hello()  //* calistir
+
+
+function helloWorld () {
+
+    console.log("Hello World")
+}
