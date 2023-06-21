2199aeb8e0fc7948726f7374871a819c8edf0284
Bunyamin Genc <bunyaming@uia.no>
2023-06-21 22:50:40 +0200

    function

--- Kodluyoruz.org/Java_script/js_17-fonction.js
+++ Kodluyoruz.org/Java_script/js_17-fonction.js
@@ -1,19 +1,55 @@
-//* ilk fonksiyon
+// //* ilk fonksiyon
 
-function hello() {
+// function hello() {
 
-    console.log("merhaba")
-    helloWorld()
+//     console.log("merhaba")
+//     helloWorld()
+// }
+
+
+// //! ic ice fonksiyon yapisi olusturabiliriz. 
+// //! foknsiyon istedigimiz yerde cagirabiliyotz 
+
+// hello()  //* calistir
+
+
+// function helloWorld () {
+
+//     console.log("Hello World")
+// }
+
+
+
+// let firstName = "lorem"
+
+// function greetings (firstName , lastName) {
+
+//     console.log (`Merhaba ${firstName} ${lastName}`)
+// }
+
+// greetings("Apple", "Appelsin")
+
+// //* paramtre vererek fonksiyonu cagirma
+
+
+//*  birden fazla parametre alan yontem
+
+
+function greetings2 (firstName, lastName){
+
+    let info =(`Merhaba ${firstName} ${lastName}`)
+    return info
+// }
+
+//* let yapisi blogun icerisnde calisiyor suslulerin icinde
+
+function domId(id, info) {
+
+
+    let domObject = document.querySelector(`#${id}`)
+
+    domId.innerHTML = info
 }
 
 
-//! ic ice fonksiyon yapisi olusturabiliriz. 
-//! foknsiyon istedigimiz yerde cagirabiliyotz 
-
-hello()  //* calistir
-
-
-function helloWorld () {
-
-    console.log("Hello World")
-}
+domId('greeting' , greetings2("Ad", "Soyad") )
