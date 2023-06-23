d15ad691a5ce8846c18ea3119673aeec7853d402
Bunyamin Genc <bunyaming@uia.no>
2023-06-23 11:27:25 +0200

    style validator

--- Projects/project_ssn/app.js
+++ Projects/project_ssn/app.js
@@ -9,12 +9,12 @@ mainForm.addEventListener("submit",(e) => {
     if (a.length === 11 && a[3] === "-" && a[6] === "-" && !a.includes(" ") && !a.includes(".")) {
         if(+(a.substring(0,3)) !== 0 && +(a.slice(0,3)) !== 666 && +(a.slice(0,3)) < 900 ){
             if (+(a.slice(4,6)) !== 0 && +(a.slice(7,11)) !== 0){
-                parag.innerHTML = `Entered SSN Number (${a}) is valid`
+                parag.innerHTML = `Entered SSN Number (${a}) is valid.`
             }
         }else {
             parag.innerText = "SSN Number must include only numbers"
         }
     } else{
-        parag.textContent = "ssn must be 11 chars and it should have two hypens(-)"
+        parag.textContent = "SSN must be 11 Charter (9 numbers and two hypens)"
     }
 })
