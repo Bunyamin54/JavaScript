be64c1ce39a39063b3e0c1a12ad2c561dbde4f79
Bunyamin Genc <bunyaming@uia.no>
2023-06-23 09:43:03 +0200

    new projects

--- Projects/project_ssn/app.js
+++ Projects/project_ssn/app.js
@@ -2,3 +2,12 @@ const input = document.querySelector("input")
 const button = document.querySelector("button")
 const paragraf = document.querySelector("p")
 
+const mainForm = document.querySelector("form")
+
+
+mainForm.addEventListener("submit", (e) => {
+e.preventDefault()
+console.log((input.value))
+
+
+})
