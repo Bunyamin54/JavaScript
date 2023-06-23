477a73b3fc29ea3e80b3393bebf786ddd128c035
Bunyamin Genc <bunyaming@uia.no>
2023-06-23 11:02:51 +0200

    preventDefault

--- Projects/project_ssn/app.js
+++ Projects/project_ssn/app.js
@@ -1,13 +1,20 @@
 const input = document.querySelector("input")
 const button = document.querySelector("button")
-const paragraf = document.querySelector("p")
-
+const parag = document.querySelector("p")
 const mainForm = document.querySelector("form")
-
-
-mainForm.addEventListener("submit", (e) => {
-e.preventDefault()
-console.log((input.value))
-
-
+mainForm.addEventListener("submit",(e) => {
+    e.preventDefault()
+    let a = input.value
+    // console.log(a);
+    if (a.length === 11 && a[3] === "-" && a[6] === "-" && !a.includes(" ") && !a.includes(".")) {
+        if(+(a.substring(0,3)) !== 0 && +(a.slice(0,3)) !== 666 && +(a.slice(0,3)) < 900 ){
+            if (+(a.slice(4,6)) !== 0 && +(a.slice(7,11)) !== 0){
+                parag.innerHTML = `Entered SSN Number (${a}) is valid`
+            }
+        }else {
+            parag.innerText = "SSN Number must include only numbers"
+        }
+    } else{
+        parag.textContent = "ssn must be 11 chars and it should have two hypens(-)"
+    }
 })
