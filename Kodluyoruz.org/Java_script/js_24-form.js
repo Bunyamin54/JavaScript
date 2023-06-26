969bd6f3221b56ddb2c1fcefa8b5d83b2ee63fa7
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 12:58:43 +0200

    localStorage

---
+++ Kodluyoruz.org/Java_script/js_24-form.js
@@ -0,0 +1,18 @@
+// *Form submit
+
+let formDOM = document.querySelector("#userForm")
+formDOM.addEventListener('submit', formSubmit)
+
+
+function formSubmit(event) {
+    event.preventDefault()  //* default islemi engelledik
+
+    console.log("islem gerceklesti.")
+
+    let scoreInputDOM = document.querySelector('#score')
+console.log(scoreInputDOM.value)   // * input icersindeki degeri value almak
+
+localStorage.setItem('score', scoreInputDOM.value)  // *locaL STORAGE EKELEMEK
+
+}
+
