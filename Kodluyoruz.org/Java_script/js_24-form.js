188389478e6b909466052056310de548063648d2
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 13:17:52 +0200

    preventDefault

--- Kodluyoruz.org/Java_script/js_24-form.js
+++ Kodluyoruz.org/Java_script/js_24-form.js
@@ -1,18 +1,29 @@
 // *Form submit
 
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit', formSubmit)
+// let formDOM = document.querySelector("#userForm")
+// formDOM.addEventListener('submit', formSubmit)
 
 
-function formSubmit(event) {
-    event.preventDefault()  //* default islemi engelledik
+// function formSubmit(event) {
+//     event.preventDefault()  //* default islemi engelledik
 
-    console.log("islem gerceklesti.")
+//     console.log("islem gerceklesti.")
 
-    let scoreInputDOM = document.querySelector('#score')
-console.log(scoreInputDOM.value)   // * input icersindeki degeri value almak
+//     let scoreInputDOM = document.querySelector('#score')
+// console.log(scoreInputDOM.value)   // * input icersindeki degeri value almak
 
-localStorage.setItem('score', scoreInputDOM.value)  // *locaL STORAGE EKELEMEK - inputdaki name bilgisi onemli
+// localStorage.setItem('score', scoreInputDOM.value)  // *locaL STORAGE EKELEMEK - inputdaki name bilgisi onemli
 
+// }
+
+// * formlar ile calismak
+
+let userFormDOM = document.querySelector("#userForm")
+userFormDOM.addEventListener('submit', formHandler)
+
+
+function formHandler(event) {
+
+
+    event.preventDefault()
 }
-
