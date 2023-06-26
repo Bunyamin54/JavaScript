50f058743992dd288b8cfa9078b74cf889acd5c9
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 13:48:40 +0200

    addevent

--- Kodluyoruz.org/Java_script/js_24-form.js
+++ Kodluyoruz.org/Java_script/js_24-form.js
@@ -23,7 +23,40 @@ userFormDOM.addEventListener('submit', formHandler)
 
 
 function formHandler(event) {
-
-
     event.preventDefault()
+ const USER_NAME = document.querySelector('#username')
+ const SCORE = document.querySelector('#score')
+ 
+ 
+ if (USER_NAME.value && SCORE.value) {
+
+    addItem(USER_NAME.value, SCORE.value)
+    USER_NAME.value = ""   //* gonderdikten sonra resetler sifirlar kulanici bilgilerini
+    SCORE.value = ""
+
+ } else {
+    console.log("Hatali Giris Yaptiniz!")
+ }
+
+}
+
+
+//* <li class="list-group-item d-flex justify-content-between align-items-start">
+//                         Content for list item
+//                       <span class="badge bg-primary rounded-pill">14</span>
+//                     </li>
+     
+let userListDOM = document.querySelector('#userList')
+
+
+
+const addItem= (userName, score) => {
+
+let liDOM = document.createElement('li')
+    liDOM.innerHTML = `   ${userName} 
+    <span class="badge bg-primary rounded-pill">${score}</span>
+    `
+
+liDOM.classList.add('list-group-item','d-flex', 'justify-content-between','align-items-start')
+    userListDOM.append(liDOM)
 }
