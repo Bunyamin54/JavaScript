79403a2d75c7bdf60622363c9f61b7e8822df95a
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 22:02:29 +0200

    countinue break methods

--- Kodluyoruz.org/Java_script/js_10-Array/loops.js
+++ Kodluyoruz.org/Java_script/js_10-Array/loops.js
@@ -33,3 +33,40 @@ const LOREM_LIST = [
 
     'lorem', 'ipsum', 'dolar' , ' amet', 'consectetur', ' adiping', 'elit', 
 ]
+
+let counter = 0
+
+// for (; counter < 10; counter++) {
+
+
+//     if(counter === 5 ) {break} //* durdurdu break ile
+//     console.log(counter)
+// }
+
+// for (; counter < 10; counter++) {
+
+
+//     if(counter === 5 ) {continue} //* 5 geleen kadar yap 5 i atla demek
+//     console.log(counter)
+// }
+
+const UL_DOM = document.querySelector('#userList')
+
+let index = 0
+
+
+// for (; index < LOREM_LIST.length; index++) {
+
+//     if(LOREM_LIST[index] =='dolar') {break}
+//     let LI_DOM = document.createElement('li')
+//     LI_DOM.innerHTML = LOREM_LIST[index]
+//     UL_DOM.appendChild(LI_DOM)
+// }
+
+for (; index < LOREM_LIST.length; index++) {
+
+    if(LOREM_LIST[index] =='dolar') {continue}
+    let LI_DOM = document.createElement('li')
+    LI_DOM.innerHTML = LOREM_LIST[index]
+    UL_DOM.appendChild(LI_DOM)
+}
