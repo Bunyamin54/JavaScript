5e7e7277063295c35f1953b0b235afeceabcb9cf
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 15:55:37 +0200

    kodluyoruz

---
+++ Kodluyoruz.org/Java_script/js_10-Array/Array.js
@@ -0,0 +1,13 @@
+let domain = "kodluyoruz"
+
+
+let isActive = false
+let items = [1, 2, 3, isActive, domain]
+
+console.log(items)
+
+let emtyArray = []  //* bos liste
+
+console.log(items.length)
+
+document.querySelector('#info').innerHTML = items.length
