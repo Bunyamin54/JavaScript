f8c54ecabd16ea21e460807a79c3d3ba0c25fa62
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 21:44:55 +0200

    users index

--- Kodluyoruz.org/Java_script/js_10-Array/loops.js
+++ Kodluyoruz.org/Java_script/js_10-Array/loops.js
@@ -3,3 +3,21 @@
 //* for lopp
 
 let users =["Lorem", "Ipsum", "Dolar"]
+
+
+for (index = 0 ; index < users.length ; index++) {
+
+    console.log(users [index])
+}
+
+// for (let index = 0 ; index < 10; index ++) {
+
+//     console.log(index)
+// }
+
+
+// let index = 0
+// for (; index < 10; index ++) {
+
+//     console.log(index)
+// }
