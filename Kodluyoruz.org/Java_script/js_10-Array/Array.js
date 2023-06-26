a01171c852eb74fdef2533006bbe60e4015362d3
Bunyamin Genc <bunyaming@uia.no>
2023-06-26 19:04:38 +0200

    items.push

--- Kodluyoruz.org/Java_script/js_10-Array/Array.js
+++ Kodluyoruz.org/Java_script/js_10-Array/Array.js
@@ -1,13 +1,40 @@
-let domain = "kodluyoruz"
+// let domain = "kodluyoruz"
 
 
-let isActive = false
-let items = [1, 2, 3, isActive, domain]
+// let isActive = false
+// let items = [15, 25, 30, isActive, domain]
 
+// console.log(items)
+
+// let emtyArray = []  //* bos liste
+
+// console.log(items.length)
+
+// document.querySelector('#info').innerHTML = items.length //* ekreana yazdirmak istersek.
+
+
+// items [0]
+
+// console.log(items[0])  //* ilk elemana ulasma
+
+// console.log(items [items.length -1])
+
+// console.log(typeof(items)) //* objekt olarak cikiyor
+
+// console.log(Array.isArray(items))  
+
+//  * hangileri isArray
+
+// console.log(Array.isArray([]))
+// console.log(Array.isArray(1))
+// console.log(Array.isArray(true))
+
+// console.log(items[Math.floor(items.length/2)]) //* ortadaki elemana ulastik 30
+
+// ** sone eleman eklemek
+let items = [10, 20, 30, 40]
 console.log(items)
 
-let emtyArray = []  //* bos liste
+items.push (50)
 
-console.log(items.length)
-
-document.querySelector('#info').innerHTML = items.length //* ekreana yazdirmak istersek.
+console.log(items)
