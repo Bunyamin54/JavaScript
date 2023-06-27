61935ab3f0672afdf965c2a51a13c7c9c7d90635
Bunyamin Genc <bunyaming@uia.no>
2023-06-27 11:29:38 +0200

    object key

---
+++ Kodluyoruz.org/Java_script/js_25-object/object.js
@@ -0,0 +1,23 @@
+// * objects
+
+// let arrayObj = [1 ,2 ,3]
+
+// let object1 = {obj: 1}
+
+// console.log("arrayObj", typeof(arrayObj))
+// console.log(object1)
+
+// let item1 = new Object ()
+// let item2 ={}
+
+// console.log(item1)
+
+let laptop1 = {
+// ** virgul ve iki nokta: koyariz
+
+    brand : "apple",
+    model : "MacBokk Pro",
+
+
+
+}
